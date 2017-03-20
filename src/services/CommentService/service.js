1e3e4cf99e42b814943088f0a9e467b4de58bba2
Sebastian Fröstl <sebastian@froestl.com>
2017-03-20 19:36:55 +0100

    fix(CommentService) return comments in promise

--- src/services/CommentService/service.js
+++ src/services/CommentService/service.js
@@ -1,3 +1,5 @@
+const COMMENTS_URI = 'http://localhost:3004/comments';
+
 export default class CommentService {
 
     static $inject = ['$http'];
@@ -6,16 +8,17 @@ export default class CommentService {
     }
 
     queryComments() {
-        return this.$http.get('http://localhost:3004/comments').then((resp) => {
-            this.setComments(resp.data)
+        return this.$http.get(COMMENTS_URI).then((resp) => {
+            return this.setComments(resp.data);
         });
     }
 
     getComments() {
-        return this.comments
+        return this.comments;
     }
 
     setComments(comments) {
-        this.comments = comments
+        this.comments = comments;
+        return comments;
     }
 }
